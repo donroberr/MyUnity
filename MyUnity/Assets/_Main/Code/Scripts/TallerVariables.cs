25000d38259965fe47723b7f3577a3758c3fc32e
Roberto <61977947+donroberr@users.noreply.github.com>
2025-08-05 11:29:45 -0500

    TallerVariables Profe

---
+++ MyUnity/Assets/_Main/Code/Scripts/TallerVariables.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class TallerVariables : MonoBehaviour
+{
+    //Declarar las variables privadas y serializadas
+
+    //nombre
+    //vida
+    //tiempo
+    //puntos
+    //estaVivo
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //le voy a dar un valor a nuestras variables
+        //de texto = cadenade texto + las variables        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
